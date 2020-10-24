faea8f1a9a8c124332c39e6baa39551d5c170c2f
Konstantinos Paidarakis <kpaidarakis@gmail.com>
2020-10-24 22:12:19 +0300

    import HttpClient module, make REST calls and display lists to UI.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component, ElementRef, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {FakeBackEndService} from './fake-back-end.service';
-import {Card} from './card';
+import {Card} from './models/card';
+import {ListService} from './services/list.service';
+import {List} from './models/list';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +12,17 @@ import {Card} from './card';
 export class AppComponent implements OnInit {
   title = 'taskManagerList';
   cards: Card[];
+  lists: List[];
   @ViewChildren('tmpCards') elementCards: QueryList<ElementRef>;
 
-  constructor(private fakeService: FakeBackEndService) {
+  constructor(private fakeService: FakeBackEndService, private listService: ListService) {
   }
 
   ngOnInit() {
     this.cards = this.fakeService.getCards();
+    this.listService.getAllLists().subscribe((lists: List[]) => {
+      this.lists = lists;
+    });
   }
 
   onClick(index: number) {
